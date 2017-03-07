5c622dcf2d6a86769ae3b994dc44d754e8492d98
Luke Woodward <luke.wdwrd@gmail.com>
2017-03-07 15:00:24 -0600

    fixed bug to call correct db; then dropped table

--- drop-table.js
+++ drop-table.js
@@ -1,10 +1,10 @@
 `use strict`;
 
 const { Database } = require('sqlite3').verbose();
-const emplDB = new Database('employees.sqlite', () => console.log("Drop Table");)
+const emplDB = new Database('exercise13.sqlite', () => console.log("Drop Table"))
 
 //deletes table
 const dropEmployees = () => {
-  myDB.run(`DROP TABLE employees`)
+  emplDB.run(`DROP TABLE more_employees`)
 }
 dropEmployees()
